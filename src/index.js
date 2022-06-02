abfd4365b44c73b354cc4a9dfa76bf8822ea7ebb
markoter <61563247+markoter@users.noreply.github.com>
2022-06-02 10:02:17 +0200

    function to calculate winner

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
-        {props.value}
+            {props.value}
         </button>
     );
 }
@@ -39,7 +39,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        }
 
         return (
             <div>
@@ -84,3 +90,23 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if (squares[a] && squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { //i believe 'squares[a] && ' part is't necessary for this logic to work
+            return squares[a];
+        }
+        return null;
+    }
+}
